a26490b076b03c467aa0d6b6ca5cc0a32af5c46e
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-07-16 00:40:14 -0400

    Removed all symbols, numbers, line breaks, dashes, and double spaces from text

--- Question-1/App.js
+++ Question-1/App.js
@@ -1,4 +1,10 @@
-// READ PARAGRAPH TEXT FILE AND SPLIT INTO SEPARATE WORDS //
+// READ PARAGRAPH TEXT FILE AND MAKE IT ALL A LOWERCASE STRING //
 const fs = require('fs');
-let words = fs.readFileSync('Paragraph.txt').toString().split(' ');
+let words = fs.readFileSync('Paragraph.txt').toString().toLowerCase();
+
+// REMOVE SYMBOLS, NUMBERS, LINE BREAKS, DASHES, AND DOUBLE SPACES //
+words = words.replace(/[,.":?0-9]/g, '');
+words = words.replace(/(\r\n|\n|\r)/gm, " ");
+words = words.replace(/-/g, " ");
+words = words.replace(/\s+/g, " ");
 console.log(words);
