1971fd996f8307dc2726aa2d0d698e34405b2b71
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-07-15 21:17:12 -0400

    Created seed data from table.csv file and seeded database

--- Question-2/television-sets.sql
+++ Question-2/television-sets.sql
@@ -1,3 +1,15 @@
 -- CREATE TABLE --
 -------------------------------------------------
--- CREATE TABLE TVPrices (TVModel VARCHAR(50), DateUpdated TIMESTAMP, Price MONEY);
+CREATE TABLE TVPrices (TVModel VARCHAR(50), DateUpdated TIMESTAMP, Price MONEY);
+
+-- SEED DATA --
+-------------------------------------------------
+INSERT INTO tvprices (tvmodel, dateupdated, price) VALUES ('Samsung v100', '2012-02-23 23:00:00', 549.99);
+INSERT INTO tvprices (tvmodel, dateupdated, price) VALUES ('Sony x300', '2012-05-22 00:00:00', 359.99);
+INSERT INTO tvprices (tvmodel, dateupdated, price) VALUES ('Samsung v100', '2013-01-22 10:20:00', 359.99);
+INSERT INTO tvprices (tvmodel, dateupdated, price) VALUES ('Samsung v100', '2013-02-22 00:00:00', 399.99);
+INSERT INTO tvprices (tvmodel, dateupdated, price) VALUES ('Sony x300', '2013-02-23 00:00:00', 329.99);
+INSERT INTO tvprices (tvmodel, dateupdated, price) VALUES ('Samsung v100', '2013-02-23 21:30:00', 639.99);
+INSERT INTO tvprices (tvmodel, dateupdated, price) VALUES ('Sony x300', '2013-05-23 22:00:00', 629.99);
+INSERT INTO tvprices (tvmodel, dateupdated, price) VALUES ('Sony x300', '2013-05-23 22:00:00', 629.99);
+INSERT INTO tvprices (tvmodel, dateupdated, price) VALUES ('Samsung z100', '2013-06-11 22:00:00', 879.99);
